74c4bdfa49e0a65a733b76ef53d625dc6b2a0772
srojasz <srojasz@gmail.com>
2019-11-18 13:56:11 +0100

    Add JS to card

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,65 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+// Paletas
+
+const palette1 = document.querySelector("#palette1");
+const palette2 = document.querySelector("#palette2");
+const palette3 = document.querySelector("#palette3");
+
+const cardContainer = document.querySelector(".js-card-container");
+
+function changeColorToPalette1() {
+  cardContainer.classList.remove("palette2");
+  cardContainer.classList.remove("palette3");
+  cardContainer.classList.add("palette1");
+}
+
+function changeColorToPalette2() {
+  cardContainer.classList.remove("palette1");
+  cardContainer.classList.remove("palette3");
+  cardContainer.classList.add("palette2");
+}
+
+function changeColorToPalette3() {
+  cardContainer.classList.remove("palette1");
+  cardContainer.classList.remove("palette2");
+  cardContainer.classList.add("palette3");
+}
+
+palette1.addEventListener("click", changeColorToPalette1);
+palette2.addEventListener("click", changeColorToPalette2);
+palette3.addEventListener("click", changeColorToPalette3);
+
+// Fill Inputs
+
+const formInputName = document.querySelector(".js-name");
+
+const formInputRole = document.querySelector(".js-role");
+
+const cardName = document.querySelector(".card__name");
+const cardRole = document.querySelector(".card__role");
+
+const defaultName = "Nombre Apellido";
+
+const defaultRole = "Front-end developer";
+
+const isFilled = ""; // No está relleno
+
+function changeName() {
+  if (formInputName.value !== isFilled) {
+    cardName.innerHTML = formInputName.value;
+  } else {
+    cardName.innerHTML = defaultName;
+  }
+}
+
+function changeRole() {
+  if (formInputRole.value !== isFilled) {
+    cardRole.innerHTML = formInputRole.value;
+  } else {
+    cardRole.innerHTML = defaultRole;
+  }
+}
+
+formInputName.addEventListener("keyup", changeName);
+formInputRole.addEventListener("keyup", changeRole);
