5ca91ef77a6f70f98646ebd7270c29f3b27cc128
marinadelafuente <mdelafuente18@gmail.com>
2019-11-25 20:33:36 +0100

    add working collapsible

--- _src/assets/js/05-collapsables.js
+++ _src/assets/js/05-collapsables.js
@@ -1,33 +1,51 @@
 'use strict';
 
-/* const triggers = document.querySelectorAll('.js-trigger');
-const content = document.querySelectorAll('.js-content');
- */
-const collapsables = document.querySelectorAll(".collapsable-box");
+const triggers = document.querySelectorAll('.js-trigger');
 
 const openCollapsable = function (ev) {
+  const currentCollapsable = ev.currentTarget.parentElement;
 
-    const currentCollapsable = ev.currentTarget.parentElement;
-    if (currentCollapsable.classList.contains("hidden")) {
-        currentCollapsable.classList.add("hidden");
-        // } else {
-        //     for (const collapsable of collapsables) {
+  for (const trigger of triggers) {
+    trigger.parentElement.classList.add('hidden');
+  }
+  if (currentCollapsable.classList.contains('hidden')) {
+    currentCollapsable.classList.remove('hidden');
+  } else {
+    currentCollapsable.classList.add('hidden');
+  }
 
-        //     currentCollapsable.classList.add("hidden");
-        // }
-    } else {
-        for (const collapsable of collapsables) {
-            collapsable.parentElement.classList.add('hidden');
-        }
-        currentCollapsable.classList.remove('hidden');
-    }
-    for (const collapsable of collapsables) {
-        collapsable.addEventListener('click', openCollapsable);
-    }
+};
+for (const trigger of triggers) {
+  trigger.addEventListener('click', openCollapsable);
 }
 
+// 'use strict';
 
+// /* const triggers = document.querySelectorAll('.js-trigger');
+// const content = document.querySelectorAll('.js-content');
+//  */
+// const collapsables = document.querySelectorAll(".collapsable-box");
 
+// const openCollapsable = function (ev) {
+
+//     const currentCollapsable = ev.currentTarget.parentElement;
+//     if (currentCollapsable.classList.contains("hidden")) {
+//         currentCollapsable.classList.add("hidden");
+//         // } else {
+//         //     for (const collapsable of collapsables) {
+
+//         //     currentCollapsable.classList.add("hidden");
+//         // }
+//     } else {
+//         for (const collapsable of collapsables) {
+//             collapsable.parentElement.classList.add('hidden');
+//         }
+//         currentCollapsable.classList.remove('hidden');
+//     }
+//     for (const collapsable of collapsables) {
+//         collapsable.addEventListener('click', openCollapsable);
+//     }
+// }
 
 // const collapsable1 = document.querySelector(".fill");
 
