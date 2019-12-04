cd79665c823beb60cea406fdbed25dfda80a69db
srojasz <srojasz@gmail.com>
2019-12-04 13:38:56 +0100

    Add api ls stil not working

--- _src/assets/js/08-api.js
+++ _src/assets/js/08-api.js
@@ -1,17 +1,14 @@
-// var submitButton = document.querySelector("#submit");
-// var responseURL = document.querySelector(".response");
-// var form = document.querySelector("form");
-// var fr = new FileReader();
+const responseURL = document.querySelector(".js-response");
 
-// submitButton.addEventListener("click", loadPhoto);
+shareButton.addEventListener("click", sendData);
 
-// function sendData() {
-//   var inputs = Array.from(form.elements);
-//   var json = getJSONFromInputs(inputs);
-//   json.skills = ["JavaScript", "React"];
-//   json.photo = fr.result;
-//   sendRequest(json);
-// }
+function sendData() {
+  var inputs = Array.from(form.elements);
+  var json = getJSONFromInputs(inputs);
+  json.skills = ["JavaScript", "React"];
+  json.photo = fr.result;
+  sendRequest(json);
+}
 
 // function loadPhoto() {
 //   var myFile = document.querySelector("#img-selector").files[0];
@@ -19,37 +16,37 @@
 //   fr.readAsDataURL(myFile);
 // }
 
-// function getJSONFromInputs(inputs) {
-//   return inputs.reduce(function(acc, val) {
-//     if (val.nodeName !== "BUTTON") acc[val.name] = val.value;
-//     return acc;
-//   }, {});
-// }
+function getJSONFromInputs(inputs) {
+  return inputs.reduce(function(acc, val) {
+    if (val.nodeName !== "BUTTON") acc[val.name] = val.value;
+    return acc;
+  }, {});
+}
 
-// function sendRequest(json) {
-//   fetch("https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/", {
-//     method: "POST",
-//     body: JSON.stringify(json),
-//     headers: {
-//       "content-type": "application/json"
-//     }
-//   })
-//     .then(function(resp) {
-//       return resp.json();
-//     })
-//     .then(function(result) {
-//       showURL(result);
-//     })
-//     .catch(function(error) {
-//       console.log(error);
-//     });
-// }
+function sendRequest(json) {
+  fetch("https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/", {
+    method: "POST",
+    body: JSON.stringify(json),
+    headers: {
+      "content-type": "application/json"
+    }
+  })
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(result) {
+      showURL(result);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
 
-// function showURL(result) {
-//   if (result.success) {
-//     responseURL.innerHTML =
-//       "<a href=" + result.cardURL + ">" + result.cardURL + "</a>";
-//   } else {
-//     responseURL.innerHTML = "ERROR:" + result.error;
-//   }
-// }
+function showURL(result) {
+  if (result.success) {
+    responseURL.innerHTML =
+      "<a href=" + result.cardURL + ">" + result.cardURL + "</a>";
+  } else {
+    responseURL.innerHTML = "ERROR:" + result.error;
+  }
+}
